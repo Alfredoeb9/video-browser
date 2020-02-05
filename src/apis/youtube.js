d3498325e53557a27d163d2621aa7298302285bd
Alfredoeb9 <alfredoeb96@gmail.com>
2020-02-04 19:50:02 -0800

    changed api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyALvCEsCmMMCm3gsNDmfb7fFyl8EaugsQI';
+const KEY = 'AIzaSyCJsLvwxdQ7q9IZhCfzPTAIAXJSPoYiOlY';
 
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: 'snippet',
+    type: 'video',
     maxResults: 5,
     key: KEY
   }
